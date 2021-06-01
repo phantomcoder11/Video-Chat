1c3d2a5a46b69dfbe1150cf5272b8b022c676572
DHRITIRAJ <dhritirajborah2020@gmail.com>
2021-06-01 12:04:24 +0530

    1st commit

---
+++ public/script.js
@@ -0,0 +1,151 @@
+
+const socket = io()
+const videoGrid = document.getElementById('VideoGrid')
+const myVideo = document.createElement('video')
+
+
+var peer = new Peer(undefined,{
+    path:'/peerjs',
+    host : '/',
+    port : 443
+
+})
+
+
+
+myVideo.muted = true
+
+let myVideoStream
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio :true
+}).then(stream =>{
+    myVideoStream = stream
+    addVideoStream(myVideo,stream)
+    
+    peer.on('call',call =>{
+        call.answer(stream)
+        const video =document.createElement('video')
+        call.on('stream',userVideoStream =>{
+            addVideoStream(video,userVideoStream)
+        })
+    })
+
+
+    socket.on('user-connected',(userId)=>{
+        connectToNewUser(userId,stream)
+         
+    })
+
+    let msg = $('input')
+$('html').keydown((e) => {
+if(e.which == 13 && msg.val().length !== 0){
+    // e.preventDefault()
+    console.log(msg.val())
+    socket.emit('message',msg.val())
+    msg.val('')
+    
+}
+})
+
+socket.on('createmessage',message => {
+$('ul').append(`<li><b class="UserT">User</b><br>${message}</li>`)
+// scrollmsg()
+})
+
+
+})
+
+peer.on('open', id=>{
+    console.log(id)
+    socket.emit('join-room',ROOM_ID,id)
+})
+
+//This part is used to call the other person
+const connectToNewUser = (userId,stream) =>{
+    // console.log('New User connected', userId)
+    const call = peer.call(userId,stream)
+    const video = document.createElement('video')
+    call.on('stream',userVideoStream =>{
+        addVideoStream(video, userVideoStream)
+    } )
+}
+
+const addVideoStream = (video,stream) =>{
+    video.srcObject = stream 
+    video.addEventListener('loadedmetadata',()=>{
+        video.play()
+    })
+    videoGrid.append(video)
+}
+
+
+//Now writing the function for scrolling messages
+const scrollmsg = ()=>{
+let x = $('message_window')
+x.scrollTop(x.prop("scrollHeight"))
+}
+
+//now the code for the mute unmute button
+const microphone = () =>{
+const enabled = myVideoStream.getAudioTracks()[0].enabled
+if(enabled){
+    myVideoStream.getAudioTracks()[0].enabled = false
+    unmuteMike()
+}
+else{
+    muteMike()
+    myVideoStream.getAudioTracks()[0].enabled = true
+    
+}
+}
+
+const unmuteMike = () =>{
+    let changes = `
+    <i class="red fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+    `
+    document.querySelector('.mute').innerHTML = changes
+}
+
+const muteMike = () =>{
+    let changes = `
+    <i class=" fas fa-microphone"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.unmute').innerHTML = changes
+}
+
+const camera = () =>{
+    
+    let enabled = myVideoStream.getVideoTracks()[0].enabled
+    if(enabled){
+        myVideoStream.getVideoTracks()[0].enabled = false
+       
+        play()
+    }
+    else{
+        stop()
+        myVideoStream.getVideoTracks()[0].enabled = true
+        
+    }
+    }
+
+
+ const stop = () =>{
+     
+    const changes = `
+    <i class=" fas fa-video"></i>
+    <span>Stop Video</span>
+    `
+    document.querySelector('.camonoff').innerHTML = changes
+ }   
+
+ const play = () =>{
+
+    const changes = `
+    <i class="red fas fa-video-slash"></i>
+    <span>Play Video</span>
+    `
+    document.querySelector('.camonoff').innerHTML = changes
+ }   
---
+++ server.js
@@ -0,0 +1,41 @@
+const express =require('express')
+const app = express()
+const server = require('http').Server(app)
+const {v4:uuidv4} = require('uuid')
+//now using peer
+const {ExpressPeerServer} = require('peer')
+const peerServer = ExpressPeerServer(server,{
+    debug:true
+})
+const port =  process.env.PORT  || 3030
+
+const io = require('socket.io')(server)
+app.set("view engine" ,"ejs")
+
+app.use(express.static('public'))
+app.use('/peerjs',peerServer)
+
+ 
+app.get('/',(req,res)=>{
+   res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId : req.params.rooms })
+})
+
+io.on('connection',socket => {
+socket.on('join-room',(roomId,userId)=>{
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected',userId)
+    socket.on('message',message=>{
+       io.to(roomId).emit('createmessage',message)
+    })
+})
+})
+
+
+
+server.listen(port,()=>{
+    console.log('Server is up running on port 3030')
+})
